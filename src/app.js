4c3a56e3c42ac9d2ac287f188609cd04b6443ec1
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2014-09-21 17:00:42 +0300

    dependencies are added

---
+++ src/app.js
@@ -0,0 +1,18 @@
+/*jshint node:true, browser: true*/
+
+var Rx          = require('rx'),
+    React       = require('react/addons'),
+    TodoStore   = require('./store/todoStore'),
+    TodoActions = require('./actions/todoActions'),
+    MainView    = require('./views/mainView.jsx');
+
+
+var todoStore = new TodoStore('react-todos');
+
+//register our actions against our store updates stream
+TodoActions.register(todoStore.updates);
+
+React.renderComponent(
+    MainView({ todoStore: todoStore }),
+    document.getElementById('todoapp')
+);
---
+++ src/static.js
@@ -0,0 +1,18 @@
+var static = require('node-static'),
+    sys = require('util');
+
+var fileServer = new static.Server('./public');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response, function (err, result) {
+            if (err) { // There was an error serving the file
+                sys.error("Error serving " + request.url + " - " + err.message);
+
+                // Respond to the client
+                response.writeHead(err.status, err.headers);
+                response.end();
+            }
+        });
+    }).resume();
+}).listen(8080);
