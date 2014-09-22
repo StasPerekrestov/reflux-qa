105cd398586da55db18c68427d697e0d1de84c10
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2014-09-22 22:24:14 +0300

    let's put it to work

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,12 @@
 /** @jsx React.DOM */
 /*jshint node:true, browser: true*/
-'use strict';
 
-var React = require('react');
+var React = require('react'),
+	Button = require('react-bootstrap').Button;
 var TestComponent =  React.createClass({
 	render: function(){
-		return  (<span>hello again</span>);
+		return  (<Button bsStyle="primary" bsSize="large">Large button</Button>);
 	}
 });
 
-React.renderComponent(new <TestComponent/>, document.getElementById("app"));
+React.renderComponent(<TestComponent/>, document.getElementById("app"));
