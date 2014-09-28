afe8f57627b892f49822cec7cf3a34a5fdeb4917
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2014-09-28 23:23:28 +0300

    gulp tasks are updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,60 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+var browserify = require('browserify');
 var reactify = require('reactify');
 var watchify = require('watchify');
+var livereload = require('gulp-livereload');
+var source = require("vinyl-source-stream");
+var watch;
 
+//see http://truongtx.me/2014/08/06/using-watchify-with-gulp-for-fast-browserify-build/
 
 gulp.task('default', function() {
-  // place code for your default task here
+	// place code for your default task here
 });
 
-// Basic usage
-gulp.task('scripts', function() {
-    // Single entry point to browserify
-    //https://gist.github.com/Sigmus/9253068
-    var bundler =  browserify({
-          insertGlobals : true,
-          debug : !gulp.env.production,
-          transform: ['reactify']
-        });
-    gulp.src('public/js/app.js')
-        .pipe(bundler)
-        .pipe(gulp.dest('./public/'))
+
+gulp.task('browserify-nowatch', function() {
+	watch = false;
+	browserifyShare();
+});
+
+gulp.task('browserify-watch', function(){
+  watch = true;
+  browserifyShare();
+});
+
+function browserifyShare() {
+	var b = browserify({
+		cache: {},
+		packageCache: {},
+		fullPaths: true,
+		transform: ['reactify']
+	});
+
+	if (watch) {
+		// if watch is enable, wrap this bundle inside watchify
+		b = watchify(b);
+		b.on('update', function() {
+			bundleShare(b);
+		});
+	}
+
+	b.add('./public/js/app.js');
+	bundleShare(b);
+};
+
+function bundleShare(b) {
+	b.bundle()
+		.pipe(source('app.js'))
+		.pipe(gulp.dest('./public'))
+		.pipe(livereload());
+}
+
+// define the browserify-watch as dependencies for this task
+gulp.task('watch', ['browserify-watch'], function() {
+	// watch other files, for example .less file
+	gulp.watch('./less/*.less', ['compile-less']);
+
+	// Start live reload server
+	livereload.listen(35729);
 });
