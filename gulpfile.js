74043ed7bc4b47bc01c5f7832de0f25db9747b3f
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2014-09-29 14:08:52 +0300

    bootstrap styles are linked now

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,12 @@ var reactify = require('reactify');
 var watchify = require('watchify');
 var livereload = require('gulp-livereload');
 var source = require("vinyl-source-stream");
+var minifyCSS = require('gulp-minify-css');
+//var clean = require('gulp-clean');
 var watch;
 
 //see http://truongtx.me/2014/08/06/using-watchify-with-gulp-for-fast-browserify-build/
+//http://mherman.org/blog/2014/08/14/kickstarting-angular-with-gulp/#.VCicCimSxX4
 
 gulp.task('default', function() {
 	// place code for your default task here
@@ -51,10 +54,22 @@ function bundleShare(b) {
 }
 
 // define the browserify-watch as dependencies for this task
-gulp.task('watch', ['browserify-watch'], function() {
+gulp.task('watch', ['browserify-watch', 'minify-css'], function() {
 	// watch other files, for example .less file
 	gulp.watch('./less/*.less', ['compile-less']);
 
 	// Start live reload server
 	livereload.listen(35729);
+});
+
+gulp.task('minify-css', function() {
+  var opts = {comments:true,spare:true};
+  gulp.src(['./bower_components/bootstrap/dist/css/bootstrap.css'])
+    .pipe(minifyCSS(opts))
+    .pipe(gulp.dest('./public/css'))
+});
+
+gulp.task('copy-bower-components', function() {
+	gulp.src('./bower_components/**')
+		.pipe(gulp.dest('public/vendor'));
 });
