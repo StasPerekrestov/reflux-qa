67ece0a515855cb95b71fa68ed8e41e1cf803042
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2014-09-29 14:27:38 +0300

    click is added

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,13 @@
 
 var React = require('react'),
 	Button = require('react-bootstrap').Button;
+
 var TestComponent =  React.createClass({
+	handleClick: function(e){
+		alert('tada');
+	},
 	render: function(){
-		return  (<Button bsStyle="primary" bsSize="large">Large button</Button>);
+		return  (<Button bsStyle="primary" bsSize="large" onClick={this.handleClick}>Large button</Button>);
 	}
 });
 
