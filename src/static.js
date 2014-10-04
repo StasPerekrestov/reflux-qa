1221b87c3cc147c6557a07794ea91b29bc038621
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2014-10-04 20:28:30 +0300

    express framework is used for the server side app

--- src/static.js
+++ src/static.js
@@ -1,19 +1,31 @@
-var static = require('node-static'),
-    sys = require('util'),
-    http = require('http');
+var express = require('express'),
+    router = express.Router(),
+    app = express(),
+    path = require('path'),
+    errorHandler = require('errorhandler');
 
-var fileServer = new static.Server('./public');
 
-http.createServer(function (request, response) {
-    request.addListener('end', function () {
-        fileServer.serve(request, response, function (err, result) {
-            if (err) { // There was an error serving the file
-                sys.error("Error serving " + request.url + " - " + err.message);
+app.set('port', process.env.PORT || 8080);
 
-                // Respond to the client
-                response.writeHead(err.status, err.headers);
-                response.end();
-            }
-        });
-    }).resume();
-}).listen(8080);
+// development only
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+}
+
+router.use(function timeLog(req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+// define the home page route
+router.get('/api', function(req, res) {
+  res.send('Birds home page');
+});
+
+app.use(express.static(path.join("./", 'public')));
+
+
+
+app.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
