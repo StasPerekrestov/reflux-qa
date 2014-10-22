9d41a96bd3d74ee5770687e62eee81eacd048610
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2014-10-22 22:18:17 +0300

    questionUpdated is sync event now. It fixes an issue with reactjs controlled input

--- public/js/qa-actions.js
+++ public/js/qa-actions.js
@@ -4,8 +4,8 @@ var QAActions = Reflux.createActions([
   'load',         // initiates the async load
   'loadComplete', // when the load is complete
   'loadError',     // when the load has failed
-  'questionUpdated',
   'changeSelectedGroup'
 ]);
 
+QAActions.questionUpdated = Reflux.createAction({sync: true});
 module.exports = QAActions;
